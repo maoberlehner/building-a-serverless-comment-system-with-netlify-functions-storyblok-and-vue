3fa7c9a73f24e1392df209a9ff717a92732d51f7
Markus Oberlehner <markus.oberlehner@gmail.com>
2018-08-04 13:48:59 +0200

    Configuring Netlify

--- vue.config.js
+++ vue.config.js
@@ -33,5 +33,13 @@ module.exports = {
       },
     },
   },
+  devServer: {
+    proxy: {
+      '/.netlify': {
+        target: `http://localhost:9000`,
+        pathRewrite: { '^/.netlify/functions': `` },
+      },
+    },
+  },
   lintOnSave: false,
 };
